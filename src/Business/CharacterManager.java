5f44bf7fa9bf7a00bcdf6d3943927bb67cfe676f
joanagutierrez <joanaguti@gmail.com>
2022-12-13 12:26:29 +0100

    Primera edicio

--- src/Business/CharacterManager.java
+++ src/Business/CharacterManager.java
@@ -9,4 +9,21 @@ public class CharacterManager {
     public CharacterManager(CharactersJsonDAO charJsonDAO) {
         this.charJsonDAO = charJsonDAO;
     }
+
+    public String changeName(String name){
+        String newName;
+        char newLetter;
+        boolean first = false;
+        for(int i=0; i<name.length(); i++){
+            if(!first){
+                newLetter = Character.toUpperCase(name.charAt(i));
+                first = true;
+            }else{
+                newLetter = Character.toLowerCase(name.charAt(i));
+            }
+            // newName = newName + newLetter;       //Como concateno un char i una cadena.
+        }
+
+        return newName;
+    }
 }
